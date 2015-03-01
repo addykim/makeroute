f6fa8710178ee92aeb05922c17fae68b43d34124
Nick Ginther <nginth@gmail.com>
2015-03-01 00:47:28 -0500

    databae why(removed database things until I know what I'm doing

--- databae/phpsqlsearch_genxml.php
+++ databae/phpsqlsearch_genxml.php
@@ -23,34 +23,14 @@ if (!$db_selected) {
 //Load csv into the database
 // TODO 
 
-	 //get the csv file 
+   //get the csv file 
     $file = 'dummy.csv'; 
     $handle = fopen($file,"r"); 
      
     //loop through the csv file and insert into database 
-    while ($data = fgetcsv($handle,1000,",","\"")) { 
-        if ($data[0]) { 
-            mysql_query("INSERT INTO markers (name, address, lat, lng, type) VALUES 
-                ( 
-                    '".addslashes($data[0])."', 
-                    '".addslashes($data[1])."', 
-                    '".addslashes($data[2])."',
-                    '".addslashes($data[3])."',
-                    '".addslashes($data[4])."'
-                ) 
-            "); 
-        } 
-    }
-
-    fclose($handle);
+    
 
 // Select all the rows in the markers table
-
-$sql = "SELECT * FROM markers WHERE 1";
-$result = mysql_query($sql);
-if (!$result) {
-  die('Invalid query: ' . mysql_error());
-}
 
 $xml = new XMLWriter();
 
@@ -60,24 +40,22 @@ $xml->setIndent(true);
 
 $xml->startElement('markers');
 
-while ($row = mysql_fetch_assoc($result)) {
-  $xml->startElement("marker");
+while ($row = fgetcsv($handle,1000,",","\"")) { 
+    $xml->startElement("marker");
+    
+    $xml->writeAttribute('name', $row[0]);
+    $xml->writeAttribute('address', $row[1]);
+    $xml->writeAttribute('lat', $row[2]);
+  $xml->writeAttribute('lng', $row[3]);
+    $xml->writeAttribute('type', $row[4]);
 
-  $xml->writeAttribute('name', $row['name']);
-  $xml->writeAttribute('address', $row['address']);
-  $xml->writeAttribute('lat', $row['lat']);
-  $xml->writeAttribute('lng', $row['lng']);
-  $xml->writeAttribute('type', $row['type']);
-
-  $xml->endElement();
+    $xml->endElement();
 }
+
+    fclose($handle);
 
 $xml->endElement();
 
 header("Content-type: text/xml");
-
-// Iterate through the rows, adding XML nodes for each
-
-
 
 ?>
